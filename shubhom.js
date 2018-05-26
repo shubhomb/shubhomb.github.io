e3a05c10acf4a24b3168e2e2d67e4005f21ca7c6
Shubhom <sb2287@cornell.edu>
2018-05-26 11:01:46 -0400

    summer update 1

--- shubhom.js
+++ shubhom.js
@@ -1,7 +1,28 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Main js file
 
- Property of Shubhom Bhattacharya
  */
+
+import anime from './animejs'
+
+var out = require('animejs')
+console.log("animejs loaded");
+
+module.exports = {
+  foo: function () {
+    var image = anime({
+      targets: '#pic1',
+      translateX: 250,
+      direction: 'alternate',
+      loop: true,
+      duration: function(el, i, l) {
+        return 1000 + (i * 1000);
+      }
+    });
+  },
+  bar: function () {
+    console.log("bar");
+  }
+};
+
+/* Make the pictures move around */
